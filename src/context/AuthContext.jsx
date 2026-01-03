a482b098b3d84b922f24bbe9678e6da9a9bc181c
adisaomoabegunde <oluwatobilobaadedapo81@gmail.com>
2026-01-03 14:07:42 +0100

    Fixxxxx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,11 @@
 import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [isAunthenticated, setIsAunthenticated] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const token = localStorage.getItem("token");
@@ -17,6 +19,7 @@ export const AuthProvider = ({ children }) => {
 
     const logout = () => {
         localStorage.removeItem("token");
+        navigate("/loginn");
         setIsAunthenticated(false);
     };
 
